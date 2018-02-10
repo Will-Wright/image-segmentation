7b02beb1b9bc76c1bd477e6a134fc7317925c986
Will-Wright <willwright@math.ucdavis.edu>
2018-02-09 20:46:16 -0800

    add image processing to ImSeg

--- src/ImSeg.py
+++ src/ImSeg.py
@@ -22,25 +22,48 @@
 # [Kressner Lu Vandereycken, 2017].
 #
 
-from cv2 import *
+import Tkinter
+from PIL import Image, ImageTk
+from sys import argv
 import numpy as np
 
-def main(image_file):
-    image = cv2.imread(image_file)
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    printBanner()
+def main(**kwargs):
+    if 'image_path' in kwargs:
+        image_path = kwargs['image_path']
+        print 'Image path provided by user: ', image_path
+    else:
+        image_path = '../test/mellon_collie_and_the_infinite_sadness_test.jpg'
+        print 'Default image chosen from path: ', image_path
+    window = Tkinter.Tk(className='Initial image')
 
+# TODO: Remove conversion to grayscale
+    # Opens image
+    image = Image.open(image_path).convert('LA')
+    image_width = image.size[0]
+    image_height = image.size[1]
+    canvas = Tkinter.Canvas(window, width=image_width, height=image_height)
+    canvas.pack()
+    image_tk = ImageTk.PhotoImage(image)
 
+    # Displays image and allows user to select pixels for image subregions
+    canvas.create_image(image_width//2, image_height//2, image=image_tk)
+    region1_array = np.zeros((100, 2))
 
+# TODO NEXT: Correctly store pixel coords
 
+#    arr_idx = 0
+    def callback(event):
+#        global arr_idx
+#        region1_array[arr_idx, :] = [event.x, event.y]
+        print 'clicked at: ', event.x, event.y
+#        arr_idx += 1
+    canvas.bind("<Button-1>", callback)
+    Tkinter.mainloop()
 
-def printBanner():
-    print("hi, bitch")
+    # Converts image to numpy array   TODO: Remove grayscale biz
+    image_array = np.array(image).astype(np.float16)[0:image_height, 0:image_width, 0]
 
-
-
-
-
+    return region1_array
 
 
 
